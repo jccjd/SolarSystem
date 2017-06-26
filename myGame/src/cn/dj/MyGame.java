743df9c993465291f4e4a308728ee9aee7d7469b
jccjd <1340944538@qq.com>
2017-06-26 14:16:16 +0800

    update

--- myGame/src/cn/dj/MyGame.java
+++ myGame/src/cn/dj/MyGame.java
@@ -12,41 +12,41 @@ import java.awt.event.WindowEvent;
  */
 public class MyGame extends Frame {
 
-    Image img = GameUtil.getImage("imgs/1.jpg");
+Image img = GameUtil.getImage("imgs/1.jpg");
 
-    public void launchFrame() {
-        setSize(500, 500);
-        setLocation(100, 100);
-        setVisible(true);
+public void launchFrame() {
+    setSize(500, 500);
+    setLocation(100, 100);
+    setVisible(true);
 
-        //移动图片
-        new PaintThread().start();
+    //移动图片
+    new PaintThread().start();
 
-        addWindowFocusListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
-    }
-    private double x=100,y=100;
-    private boolean left,up;
+    addWindowFocusListener(new WindowAdapter() {
+        @Override
+        public void windowClosing(WindowEvent e) {
+            System.exit(0);
+        }
+    });
+}
+private double x=100,y=100;
+private boolean left,up;
 
-    @Override
-    public void paint(Graphics g){
-        g.drawRect(100,100,300,300);
-        g.drawLine(100,100,200,200);
-        g.setColor(Color.cyan);
-        g.drawOval(100,100,200,200);
-        g.setColor(Color.BLUE);
-        Font f = new Font("楷体",Font.BOLD,100);
-        g.setFont(f);
-        g.drawString("你好",200,200);
-        g.setColor(Color.cyan);
+@Override
+public void paint(Graphics g){
+    g.drawRect(100,100,300,300);
+    g.drawLine(100,100,200,200);
+    g.setColor(Color.cyan);
+    g.drawOval(100,100,200,200);
+    g.setColor(Color.BLUE);
+    Font f = new Font("楷体",Font.BOLD,100);
+    g.setFont(f);
+    g.drawString("你好",200,200);
+    g.setColor(Color.cyan);
 
-        g.fillOval(100,100,100,100);
+    g.fillOval((int) x, (int) y,100,100);
 
-        g.drawImage(img, (int) x, (int) y, null);
+    g.drawImage(img, (int) x, (int) y, null);
 //
 //        if (left) {
 //            x -= 30;
@@ -61,40 +61,40 @@ public class MyGame extends Frame {
 //            left = false;
 //        }
 
-        if (up) {
-            y -= 3;
+    if (up) {
+        y -= 3;
 
-        } else {
-            y += 3;
-        }
-        if (y>500-30) {
-            up = true;
-        }
-        if (y<30) {
-            up = false;
-        }
+    } else {
+        y += 3;
     }
-    //移动图片
-    class PaintThread extends Thread{
-        public void run() {
-            while (true) {
+    if (y>500-30) {
+        up = true;
+    }
+    if (y<30) {
+        up = false;
+    }
+}
+//移动图片
+class PaintThread extends Thread{
+    public void run() {
+        while (true) {
 
-                repaint();
+            repaint();
 
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
-    //测试
-    public static void main(String[] args) {
-        MyGame gf = new MyGame();
-        gf.launchFrame();
+}
+//测试
+public static void main(String[] args) {
+    MyGame gf = new MyGame();
+    gf.launchFrame();
 
-    }
+}
 
 }
 
--- myGame/src/practice/MyGame.java
+++ myGame/src/practice/MyGame.java
@@ -7,44 +7,43 @@ import java.awt.event.WindowEvent;
 /**
  * Created by lenovo on 2017/6/24.
  */
-
 public class MyGame extends Frame {
-    public void launchFrame() {
-        setSize(500,500);
-        setLocation(200,200);
-        setVisible(true);
-
-        new PaintThread().start();
-
-        addWindowFocusListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
-    }
-    private double x=100,y=100;
-    private boolean up;
-    public void paint(Graphics g) {
-        g.setColor(Color.cyan);
-        g.drawOval((int)x,(int)y,100,100);
-
-        if (up) {
-            y -= 10;
-        } else {
-            y += 10;
-        }
-        if (y>500-30){
-            up = true;
-        }
-        if (y<30){
-            up = false;
-        }
-    }
-    class PaintThread extends Thread{
+    public void launchFrame (){
+    setSize(500,500);
+    setLocation(100,100);
+    setVisible(true);
+//
+    new PaintThread().start();
+    addWindowFocusListener(new WindowAdapter() {
         @Override
-        public void run() {
+        public void windowClosing(WindowEvent e) {
+            System.exit(0);
+        }
+    });
+}
+private double x=100,y=100;
+private boolean up;
+public void paint (Graphics g) {
+    g.drawRect((int)x,(int)y,100,100);
+
+    if (up) {
+        x -= 10;
+    } else {
+        x += 10;
+    }
+    if (x > 500-30) {
+        up = true;
+    }
+    if (x <30) {
+        up = false;
+    }
+}
+class PaintThread extends Thread {
+    @Override
+    public void run() {
+        while (true) {
             repaint();
+
             try {
                 Thread.sleep(40);
             } catch (InterruptedException e) {
@@ -52,10 +51,10 @@ public class MyGame extends Frame {
             }
         }
     }
+}
 
-    public static void main(String[] args) {
-        MyGame gf = new MyGame();
-        gf.launchFrame();
-    }
-
+public static void main(String[] args) {
+    MyGame mg = new MyGame();
+    mg.launchFrame();
+}
 }
